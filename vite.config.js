ba1158f2444cfdfdec107a2e659b8feb68580f62
sandeep <sandeepfullstackjava1@gmail.com>
2026-03-23 15:39:56 +0530

    add backend url

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'https://course-backend-m1i2.onrender.com',
         changeOrigin: true,
       },
     },
