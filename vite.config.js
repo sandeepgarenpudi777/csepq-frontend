374efd46ff7fdef2ad24974fc909aefbcb818ea2
Sandeep Garnepudi <125200913+sandeepgarenpudi777@users.noreply.github.com>
2026-03-30 12:01:59 +0530

    Update API proxy target in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'https://online-course-backend-8kcy.onrender.com',
+        target: 'https://csepq-backend.onrender.com',
         changeOrigin: true,
       },
     },
